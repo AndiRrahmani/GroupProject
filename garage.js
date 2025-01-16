16bed830127acc74bde0970bc7268c8cc3ea4d1d
anik60000 <aaanikuu@gmail.com>
2025-01-16 16:00:21 +0100

    kodi i anikut

--- garage.js
+++ garage.js
@@ -1,13 +1,16 @@
-
 document.getElementById('buyButton').addEventListener('click', function(event) {
     event.preventDefault(); 
     var email = prompt('Please enter your email address:');
     if (email) {
-        var confirmation = confirm('Are you sure you want to proceed with the purchase?');
-        if (confirmation) {
-            alert('Thank you for your purchase! A confirmation email will be sent to ' + email);  
+        if (email.endsWith('@gmail.com')) {
+            var confirmation = confirm('Are you sure you want to proceed with the purchase?');
+            if (confirmation) {
+                alert('Thank you for your purchase! A confirmation email will be sent to ' + email);  
+            } else {
+                alert('Purchase canceled.');
+            }
         } else {
-            alert('Purchase canceled.');
+            alert('Please enter a valid gmail account ending with @gmail.com');
         }
     } else {
         alert('Email is required to proceed.');
